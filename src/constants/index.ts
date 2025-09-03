aaaffbdd839c2d6b5840cb9f66797f47771f92d6
CameronFoxly <cameronfoxly@github.comm>
2025-09-03 12:33:13 -0700

    Fixed console issue with dupkicate colors

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -52,7 +52,7 @@ export const DEFAULT_COLORS = [
   "#000000", "#FFFFFF", "#FF0000", "#00FF00", "#0000FF",
   "#FFFF00", "#FF00FF", "#00FFFF", "#808080", "#C0C0C0",
   "#800000", "#008000", "#000080", "#808000", "#800080",
-  "#008080", "#FFA500", "#FFC0CB", "#A52A2A", "#800000"
+  "#008080", "#FFA500", "#FFC0CB", "#A52A2A"
 ] as const;
 
 export const EXPORT_FORMATS = [
